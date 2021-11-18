206dba6fbef5c3568514a7976eec8bf4cfa3badc
Davi <davi.freitas@useall.com.br>
2021-11-18 12:53:00 -0300

    @emit

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="$emit('papoReto')">Trocar imagem de perfil</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MudarImagem",
+  emits: ['papoReto'],
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/form/Picture.vue
+++ src/components/form/Picture.vue
@@ -1,15 +1,25 @@
 <template>
     <img  :src="avatar" :alt="descricao" >
+  <MudarImagem @papo-reto="trocarImagem" />
 </template>
 
 <script>
+    import MudarImagem from "@/components/MudarImagem";
     export default {
-        name: "Picture",
-        data () {
+      name: "Picture",
+      components: {
+          MudarImagem
+      },
+      data () {
             return {
                 avatar: "/img/avatar.png",
                 descricao: "Usuario"
             }
+      },
+      methods: {
+        trocarImagem() {
+          this.avatar = "/img/avatar2.jpg"
         }
+      }
     }
 </script>
